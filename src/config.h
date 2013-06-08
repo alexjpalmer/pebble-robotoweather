49013f3b9676aff34020b209018d1b9dba1d241d
alexjpalmer <alexjpalmer@gmail.com>
2013-06-08 12:58:13 +0200

    Update config.h
    Define refresh times

--- src/config.h
+++ src/config.h
@@ -1,4 +1,6 @@
 // Any of "us", "ca", "uk" (for idiosyncratic US, Candian and British measurements),
 // "si" (for pure metric) or "auto" (determined by the above latitude/longitude)
 #define UNIT_SYSTEM "auto"
+#define PING_DELAY 3
+#define WEATHER_DELAY 20
 //#define DEBUG
