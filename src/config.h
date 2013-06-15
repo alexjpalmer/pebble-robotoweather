0d8b7bbbc6f6db5411914bdc24d1ceeabe2dad14
alexjpalmer <alexjpalmer@gmail.com>
2013-06-15 15:37:29 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -2,13 +2,15 @@
 #define CONFIG_H
 
 //NOTE: longitude is positive for East and negative for West
-#define LATITUDE      			51.0
-#define LONGITUDE 					8.0
-#define TIMEZONE 					0
-#define DAY_NAME_LANGUAGE 			DAY_NAME_ENGLISH 		// Valid values: DAY_NAME_ENGLISH, DAY_NAME_GERMAN, DAY_NAME_FRENCH
-#define MOONPHASE_NAME_LANGUAGE 	MOONPHASE_TEXT_ENGLISH 	// Valid values: MOONPHASE_TEXT_ENGLISH, MOONPHASE_TEXT_GERMAN, MOONPHASE_TEXT_FRENCH
-#define day_month_x 				day_month_day_first 	// Valid values: day_month_month_first, day_month_day_first
-#define TRANSLATION_CW 				"CW%V" 					// Translation for the calendar week (e.g. "CW%V")
+#define LATITUDE      		51.0
+#define LONGITUDE 		8.0
+#define TIMEZONE 		0
+#define DAY_NAME_LANGUAGE 	DAY_NAME_ENGLISH 	// Valid values: DAY_NAME_ENGLISH, DAY_NAME_GERMAN, DAY_NAME_FRENCH
+#define MOONPHASE_NAME_LANGUAGE MOONPHASE_TEXT_ENGLISH 	// Valid values: MOONPHASE_TEXT_ENGLISH, MOONPHASE_TEXT_GERMAN, MOONPHASE_TEXT_FRENCH
+#define day_month_x 		day_month_day_first 	// Valid values: day_month_month_first, day_month_day_first
+#define TRANSLATION_CW 		"CW%V" 			// Translation for the calendar week (e.g. "CW%V")
+#define WEATHER_DELAY 		20			// Minute delay between weather updates 
+#define PING_DELAY		3			// Minute delay between check phone connected updates
 
 // Any of "us", "ca", "uk" (for idiosyncratic US, Candian and British measurements),
 // "si" (for pure metric) or "auto" (determined by the above latitude/longitude)
